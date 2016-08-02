b8b0c849233b6620629c82ec99b0430901e8c208
Alexandre Marcireau <alexandre.marcireau@student.ecp.fr>
2016-08-02 12:07:02 +0200

    Use the same values as in the documentation

--- test/selectRectangleTest.cpp
+++ test/selectRectangleTest.cpp
@@ -8,9 +8,9 @@ struct Event {
 };
 
 TEST_CASE("Filter out events outside the rectangle", "[SelectRectangle]") {
-    auto selectRectangle = tarsier::make_selectRectangle<Event>(20, 20, 100, 100, [](Event event) -> void {
+    auto selectRectangle = tarsier::make_selectRectangle<Event>(50, 50, 204, 140, [](Event event) -> void {
         REQUIRE(event.x == 100);
     });
-    selectRectangle(Event{200, 200});
+    selectRectangle(Event{300, 200});
     selectRectangle(Event{100, 100});
 }
