18cf3c03401daaaba18583e345fdee6687df4e7e
Alexandre Marcireau <alexandre.marcireau@student.ecp.fr>
2016-11-17 16:45:06 +0100

    Add the flow skeleton

---
+++ source/computeFlow.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+/// tarsier is a collection of event handlers.
+namespace tarsier {
+
+    /// Flow represents the parameters of an optical flow change.
+    struct Flow {
+
+        /// x represents the coordinate of the flow on the sensor grid alongside the horizontal axis.
+        /// x is 0 on the left, and increases from left to right.
+        uint16_t x;
+
+        /// y represents the coordinate of the flow on the sensor grid alongside the vertical axis.
+        /// y is 0 on the bottom, and increases bottom to top.
+        uint16_t y;
+
+        /// theta represents the flow angle in radians, with the horizontal axis as reference.
+        /// theta is in the interval ]-Pi, Pi]
+        double theta;
+
+        /// intensity represents the flow intensity in pixels per microsecond.
+        double intensity;
+    }
+
+    /// ComputeFlow evaluates the optical flow.
+    template <typename Event, typename HandleFlow>
+    class ComputeFlow {
+
+        public:
+            ComputeFlow(std::size_t width, std::size_t height, HandleFlow handleFlow) :
+                _width(width),
+                _height(height),
+                _handleFlow(std::forward<HandleFlow>(handleFlow)),
+                _timestamps(width * height, std::numeric_limits<int64_t>::lowest())
+            {
+            }
+            ComputeFlow(const ComputeFlow&) = delete;
+            ComputeFlow(ComputeFlow&&) = default;
+            ComputeFlow& operator=(const ComputeFlow&) = delete;
+            ComputeFlow& operator=(ComputeFlow&&) = default;
+            virtual ~ComputeFlow() {}
+
+            /// operator() handles an event.
+            virtual void operator()(Event event) {
+                _handleFlow(/* computed flow goes here*/);
+            }
+
+        protected:
+            const std::size_t _width;
+            const std::size_t _height;
+            HandleFlow _handleFlow;
+    }
+}
