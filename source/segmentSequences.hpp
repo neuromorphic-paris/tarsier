7bcfb7bf1180aea8d08c1ac4a240508dcbd4227d
lilk <lilk@slave>
2017-06-07 18:10:18 +0200

    [segmentSequences] a class and the make_ associated with the purpose of build segmentation by scanning the event flow two handlers: one able to propagate a sequence when it is ok and do anything with it one able to propagate the event and do anything with it

---
+++ source/segmentSequences.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <vector>
+
+/// tarsier is a collection of event handler
+namespace tarsier{
+  template<
+    typename Event,// must be contain at least a field .sequenceTrigger
+    typename HandlerSequences,
+    typename HandlerSegmentSequences>
+  class SegmentSequences{
+  public:
+    SegmentSequences(HandlerSequences handlerSequences,
+                     HandlerSegmentSequences handlerSegmentSequences):
+      _handlerSequences(std::forward<HandlerSequences>(handlerSequences)),
+      _handlerSegmentSequences(std::forward<HandlerSegmentSequences>(handlerSegmentSequences)),
+      _stackEvent(false)
+    {}
+
+    virtual ~SegmentSequences(){}
+
+    virtual void operator()(Event ev){
+      if(ev.sequenceTrigger == 1){
+        _curentSequence.clear();
+      }else if(ev.sequenceTrigger == -1){
+        _handlerSequences(_curentSequence);
+      }
+
+      if(_stackEvent){
+        _curentSequence.push_back(ev);
+      }
+
+      _handlerSegmentSequences(ev);
+    }
+
+  protected:
+    HandlerSequences _handlerSequences;
+    HandlerSegmentSequences _handlerSegmentSequences;
+    std::vector<Event> _curentSequence;
+
+    bool _stackEvent;
+  };
+
+  /// make_segmentSequences
+  template<
+    typename Event,// must be contain at least a field .sequenceTrigger
+    typename HandlerSequences,
+    typename HandlerSegmentSequences>
+  SegmentSequences<Event,
+                   HandlerSequences,
+                   HandlerSegmentSequences>
+  make_segmentSequences(HandlerSequences handlerSequences,
+                        HandlerSegmentSequences handlerSegmentSequences)
+  {
+    return SegmentSequences<Event,
+                            HandlerSequences,
+                            HandlerSegmentSequences>
+      (std::forward<HandlerSequences>(handlerSequences),
+       std::forward<HandlerSegmentSequences>(handlerSegmentSequences)
+       );
+  }
+};
