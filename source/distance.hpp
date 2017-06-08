e9cd7352e8b12e3a193eeb27625c7087d180342f
lilk <lilk@slave>
2017-06-08 12:07:36 +0200

    [distance] levenshtein functions become classes could be contructed using chosen cost for add/delete/substitute

--- source/distance.hpp
+++ source/distance.hpp
@@ -6,49 +6,93 @@
 /// tarsier is a collection of event handler
 namespace tarsier{
   /// computeLevenshteinDistance is able to estimate the edit distance between two sequences according the classic cost function
-  template<typename FisrtContainer, // need at least operator[] that return a single value
-           typename SecondContainer> // need at least operator[] that return a single value
-  std::size_t computeLevenshteinDistance(FisrtContainer inputString,
-                                         SecondContainer expectedString){
-    std::vector<std::vector<std::size_t> > d(inputString.size()+1, std::vector<std::size_t>(expectedString.size()+1,0));
-    std::size_t substituteCost = 0, addCost = 0, deleteCost = 0;
-    for(std::size_t i = 0; i <= inputString.size(); i++){
-      d[i][0] = i;
-    }
-    for(std::size_t i = 1; i <= expectedString.size(); i++){
-      d[0][i] = i;
-    }
-    for(std::size_t i = 1; i <= inputString.size(); i++){
-      for(std::size_t j = 1; j <= expectedString.size(); j++){
-        substituteCost = d[i-1][j-1] + ((inputString[i-1] == expectedString[j-1]) ? 0 : 1);
-        addCost = d[i][j-1] + 1;
-        deleteCost = d[i-1][j] + 1;
-        d[i][j] = (addCost < deleteCost) ? ((addCost < substituteCost) ? addCost : substituteCost) : ((deleteCost < substituteCost) ? deleteCost : substituteCost);
+  template<typename FisrtContainer, // need at least operator[] that return a single value and .size()
+           typename SecondContainer> // need at least operator[] that return a single value and .size()
+  class computeLevenshteinDistance{
+  public:
+    computeLevenshteinDistance(double addCost=1,
+                               double deleteCost=1,
+                               double substituteCost=1):
+      _addCost(addCost),
+      _deleteCost(deleteCost),
+      _substituteCost(substituteCost)
+    {}
+
+    virtual ~computeLevenshteinDistance(){}
+
+    virtual double operator()(FisrtContainer inputString,
+                              SecondContainer expectedString
+                              ){
+      std::vector<std::vector<double> > d(inputString.size()+1, std::vector<double>(expectedString.size()+1,0));
+      double substituteElem = 0., addElem = 0., deleteElem = 0.;
+
+      for(std::size_t i = 0; i <= inputString.size(); i++){
+        d[i][0] = i;
       }
+      for(std::size_t i = 1; i <= expectedString.size(); i++){
+        d[0][i] = i;
+      }
+      for(std::size_t i = 1; i <= inputString.size(); i++){
+        for(std::size_t j = 1; j <= expectedString.size(); j++){
+          substituteElem = d[i-1][j-1] + ((inputString[i-1] == expectedString[j-1]) ? 0 : _substituteCost);
+          addElem = d[i][j-1] + _addCost;
+          deleteElem = d[i-1][j] + _deleteCost;
+          d[i][j] = (addElem < deleteElem) ? ((addElem < substituteElem) ? addElem : substituteElem) : ((deleteElem < substituteElem) ? deleteElem : substituteElem);
+        }
+      }
+      return d[inputString.size()][expectedString.size()];
     }
-    return d[inputString.size()][expectedString.size()];
-  }
+
+  protected:
+    double _addCost;
+    double _deleteCost;
+    double _substituteCost;
+  };
 
   /// computeLevenshteinDistanceHeuristic is able to estimate the edit distance between two sequences according an heuristic that allow to be invariant to repetitions
-  template<typename FisrtContainer, // need at least operator[] that return a single value
-           typename SecondContainer> // need at least operator[] that return a single value
-  std::size_t computeLevenshteinDistanceHeuristic(FisrtContainer inputString,
-                                                    SecondContainer expectedString){
-    std::vector<std::vector<std::size_t> > d(inputString.size(), std::vector<std::size_t>(expectedString.size(),0));
-    d[0][0] = ((inputString[0] == expectedString[0]) ? 0 : 1);
-    for(std::size_t i = 1; i < inputString.size(); i++){
-      d[i][0] = d[i-1][0] + ((inputString[i] == expectedString[0]) ? 0 : 1);
-    }
-    for(std::size_t i = 1; i < expectedString.size(); i++){
-      d[0][i] = d[0][i-1] + ((inputString[0] == expectedString[i]) ? 0 : 1);
-    }
-    for(std::size_t i = 1; i < inputString.size(); i++){
-      for(std::size_t j = 1; j < expectedString.size(); j++){
-        d[i][j]= ((inputString[i] == expectedString[j]) ? 0 : 1) + ((d[i][j-1] < d[i-1][j]) ? ((d[i][j-1] < d[i-1][j-1]) ? d[i][j-1] : d[i-1][j-1]) : ((d[i-1][j] < d[i-1][j-1]) ? d[i-1][j] : d[i-1][j-1]));
+  template<typename FisrtContainer, // need at least operator[] that return a single value and .size()
+           typename SecondContainer> // need at least operator[] that return a single value and .size()
+  class computeLevenshteinDistanceHeuristic{
+  public:
+    computeLevenshteinDistanceHeuristic(double addCost=0,
+                                        double deleteCost=0,
+                                        double substituteCost=0):
+      _addCost(addCost),
+      _deleteCost(deleteCost),
+      _substituteCost(substituteCost)
+    {}
+
+    virtual ~computeLevenshteinDistanceHeuristic(){}
+
+    virtual double operator()(FisrtContainer inputString,
+                              SecondContainer expectedString
+                              ){
+      std::vector<std::vector<double> > d(inputString.size(), std::vector<double>(expectedString.size(),0));
+      double substituteElem = 0., addElem = 0., deleteElem = 0.;
+
+      d[0][0] = ((inputString[0] == expectedString[0]) ? 0 : 1);
+      for(std::size_t i = 1; i < inputString.size(); i++){
+        d[i][0] = d[i-1][0] + ((inputString[i] == expectedString[0]) ? 0 : 1);
       }
+      for(std::size_t i = 1; i < expectedString.size(); i++){
+        d[0][i] = d[0][i-1] + ((inputString[0] == expectedString[i]) ? 0 : 1);
+      }
+      for(std::size_t i = 1; i < inputString.size(); i++){
+        for(std::size_t j = 1; j < expectedString.size(); j++){
+          substituteElem = d[i-1][j-1]+_substituteCost;
+          addElem = d[i][j-1]+_addCost;
+          deleteElem = d[i-1][j]+_deleteCost;
+          d[i][j]= ((inputString[i] == expectedString[j]) ? 0 : 1) + ((addElem < deleteElem) ? ((addElem < substituteElem) ? addElem : substituteElem) : ((deleteElem < substituteElem) ? deleteElem : substituteElem));
+        }
+      }
+      return d[inputString.size()-1][expectedString.size()-1];
     }
-    return d[inputString.size()-1][expectedString.size()-1];
-  }
+
+  protected:
+    double _addCost;
+    double _deleteCost;
+    double _substituteCost;
+  };
 
   /// Implementation of euclidean distance better on vectors upper than 20 elems
   template<typename ContainerIterator,typename T>
