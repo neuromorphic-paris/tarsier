ba98d80e3b8ac3ba8ff587cffd2e4efed6c5b712
lilk <lilk@slave>
2017-06-09 14:56:28 +0200

    [groupsSelector] free of bug, access of elem of the list "ok" with iterator

--- source/groupsSelector.hpp
+++ source/groupsSelector.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <list>
+#include <vector>
+#include <algorithm>
 
 namespace tarsier{
   class GroupsSelector{
@@ -23,29 +25,52 @@ namespace tarsier{
             }
           }
           if(alreadyHere == false){
-            _thresholds.push(back);
+            _thresholds.push_back(it2);
           }
         }
       }
       std::sort(_thresholds.begin(), _thresholds.end());
+      /*for(auto&& it: _thresholds){
+        std::cout << it << "\t";
+        }
+        std::cout << std::endl;
+        std::cout << std::endl;*/
+
+
+      for(auto&& itThd: _thresholds){
+        _groups = this->reduce(matrix, itThd);
+        for(auto&& it: _groups){
+          for(auto&& it2: it){
+            std::cout << it2 << "\t";
+          }
+          std::cout << std::endl;
+        }
+        std::cout << std::endl;
+        std::cout << std::endl;
+      }
+    }
+
+  protected:
+    std::list<std::vector<std::size_t> > reduce(std::vector<std::vector<double> > matrix, double thd){
+      std::list<std::vector<std::size_t> > groups;
 
       for(auto&& it: matrix){
         std::vector<std::size_t> idx;
         std::size_t cpt = 0;
         for(auto&& it2: it){
-          if(it2 <= _thresholds[0]){
+          if(it2 <= thd){
             idx.push_back(cpt);
           }
           cpt++;
         }
         std::vector<std::size_t> clusterToMerged;
         cpt = 0;
-        for(auto&& itGroups: _groups){
+        for(auto&& itGroups: groups){
           bool nextGroup = false;
           for(auto&& itIdx: idx){
             for(auto&& it2Groups: itGroups){
               if(itIdx == it2Groups){
-                clusterToMerged.push(cpt);
+                clusterToMerged.push_back(cpt);
                 nextGroup = true;
                 break;
               }
@@ -56,23 +81,25 @@ namespace tarsier{
           }
           cpt++;
         }
-
         if(clusterToMerged.size() == 0){
-          _groups.push_back(idx);
+          groups.push_back(idx);
         }else{
-          _groups[clusterToMerged[0]].insert(_groups[clusterToMerged[0]].begin(), idx.begin(), idx.end());
-          for(auto i = 1; i < clusterToMerged.size()){
-            _groups[clusterToMerged[0]].insert(_groups[clusterToMerged[0]].begin(),
-                                               _groups[clusterToMerged[i]].begin(),
-                                               _groups[clusterToMerged[i]].end());
+          auto selectedGroup = std::next(groups.begin(),clusterToMerged[0]);
+          selectedGroup->insert(selectedGroup->begin(), idx.begin(), idx.end());
+          for(auto i = clusterToMerged.size()-1; i > 0; i--){
+            auto temp = std::next(groups.begin(),clusterToMerged[i]);
+            selectedGroup->insert(selectedGroup->begin(),temp->begin(),temp->end());
+            groups.erase(temp);
           }
-          std::sort(_groups[clusterToMerged[0]].begin(), _groups[clusterToMerged[0]].end());
-          std::unique(_groups[clusterToMerged[0]].begin(), _groups[clusterToMerged[0]].end());
+
+          std::sort(selectedGroup->begin(), selectedGroup->end());
+          auto temp = std::unique(selectedGroup->begin(), selectedGroup->end());
+          selectedGroup->resize(std::distance(selectedGroup->begin(), temp));
         }
       }
+      return groups;
     }
 
-  protected:
     std::list<std::vector<std::size_t> > _groups;
     std::vector<double> _thresholds;
   };
