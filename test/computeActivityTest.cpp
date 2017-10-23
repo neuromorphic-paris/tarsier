2d46b59f8e294ab3599add1480e4a066337d250b
Gregor Lenz <lenz.gregor@gmail.com>
2017-10-23 14:22:03 +0200

    added test cases for separate ON and OFF activities

--- test/computeActivityTest.cpp
+++ test/computeActivityTest.cpp
@@ -4,40 +4,62 @@
 
 struct Event {
   uint64_t timestamp;
+  bool polarity;
 } __attribute__((packed));
 
 struct ActivityEvent {
   uint64_t timestamp;
   double activity;
+  double activityON;
+  double activityOFF;
 };
 
 const uint64_t lifespan = 30000;
 
-std::vector<double> expectedActivities;
+std::vector<double> expectedActivitiesON;
+std::vector<double> expectedActivitiesOFF;
 
 TEST_CASE("Compute the event activity in a scene", "[ComputeActivity]") {
   auto activityEventGenerated = false;
   auto computeActivity =
       tarsier::make_computeActivity<Event, ActivityEvent, lifespan>(
-          [](Event event, double activity) -> ActivityEvent {
-            return ActivityEvent{event.timestamp, activity};
+          [](Event event, double activity, double activityON,
+             double activityOFF) -> ActivityEvent {
+            return ActivityEvent{event.timestamp, activity, activityON,
+                                 activityOFF};
           },
           [&activityEventGenerated](ActivityEvent activityEvent) -> void {
             activityEventGenerated = true;
-            expectedActivities.push_back(activityEvent.activity);
+            expectedActivitiesON.push_back(activityEvent.activityON);
+            expectedActivitiesOFF.push_back(activityEvent.activityOFF);
           });
 
   // Activity = 1
-  computeActivity(Event{10000});
-  REQUIRE(std::abs(expectedActivities[0] - 1.) < 0.001);
+  computeActivity(Event{10000, true});
+  REQUIRE(std::abs(expectedActivitiesON[0] - 1.) < 0.001);
+  REQUIRE(expectedActivitiesOFF[0] == 0);
+
+  computeActivity(Event{15000, false});
+  REQUIRE(std::abs(expectedActivitiesON[1] - 1.) < 0.001);
+  REQUIRE(std::abs(expectedActivitiesOFF[1] - 1.) < 0.001);
 
   // Activity = 1.36787944117
-  computeActivity(Event{40000});
-  REQUIRE(std::abs(expectedActivities[1] - 1.36787944117) < 0.001);
+  computeActivity(Event{40000, true});
+  REQUIRE(std::abs(expectedActivitiesON[2] - 1.36787944117) < 0.001);
+  REQUIRE(std::abs(expectedActivitiesOFF[2] - 1.) < 0.001);
+
+  computeActivity(Event{45000, false});
+  REQUIRE(std::abs(expectedActivitiesON[3] - 1.36787944117) < 0.001);
+  REQUIRE(std::abs(expectedActivitiesOFF[3] - 1.36787944117) < 0.001);
 
   // Activity = 1.50321472441
-  computeActivity(Event{70000});
-  REQUIRE(std::abs(expectedActivities[2] - 1.50321472441) < 0.001);
+  computeActivity(Event{70000, true});
+  REQUIRE(std::abs(expectedActivitiesON[4] - 1.50321472441) < 0.001);
+  REQUIRE(std::abs(expectedActivitiesOFF[4] - 1.36787944117) < 0.001);
+
+  computeActivity(Event{75000, false});
+  REQUIRE(std::abs(expectedActivitiesON[5] - 1.50321472441) < 0.001);
+  REQUIRE(std::abs(expectedActivitiesOFF[5] - 1.50321472441) < 0.001);
 
   REQUIRE(activityEventGenerated);
 }
