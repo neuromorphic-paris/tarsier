5ab2cfd6f2f3f126859ed757126a0cb62975e28d
Alexandre Marcireau <alexandre.marcireau@inserm.fr>
2018-05-24 10:52:02 +0200

    Add an event handler template generator

--- premake4.lua
+++ premake4.lua
@@ -1,3 +1,22 @@
+local template = require 'template'
+
+newaction {
+    trigger = 'template',
+    description = 'generate a template event handler',
+    execute = function()
+        if _OPTIONS['configuration'] == nil then
+            template('configuration.json')
+        else
+            template(_OPTIONS['configuration'])
+        end
+    end
+}
+newoption {
+    trigger = 'configuration',
+    value = '/path/to/configuration.json',
+    description = 'set the path to the configuration file for template generation'
+}
+
 solution 'tarsier'
     configurations {'release', 'debug'}
     location 'build'
