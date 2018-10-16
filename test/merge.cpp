61b1b439c7b0ebe3caf5d8d48c7c18fe7228868b
Alexandre Marcireau <alexandre.marcireau@gmail.com>
2018-10-16 11:24:24 +0200

    Use circular fifos in the merge implementation

--- test/merge.cpp
+++ test/merge.cpp
@@ -7,7 +7,7 @@ struct event {
 
 TEST_CASE("Merge two streams", "[merge]") {
     std::size_t index = 0;
-    auto merge = tarsier::make_merge<2, event>([&](event event) -> void {
+    auto merge = tarsier::make_merge<2, event>(256, std::chrono::milliseconds(20), [&](event event) -> void {
         REQUIRE(event.t == index);
         ++index;
     });
